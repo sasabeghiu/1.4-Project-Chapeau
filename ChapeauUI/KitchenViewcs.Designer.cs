8bee4370c329a52ca663da0aef6a2939967936c8
Pablo Gulias <pabloguliasprats@gmail.com>
2021-06-10 21:45:14 +0200

    Created KitchenView form

---
+++ ChapeauUI/KitchenViewcs.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace ChapeauUI
+{
+    partial class KitchenViewcs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Id = new System.Windows.Forms.ColumnHeader();
+            this.status = new System.Windows.Forms.ColumnHeader();
+            this.time = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Id,
+            this.status,
+            this.time});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 126);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(440, 312);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Id
+            // 
+            this.Id.Text = "Order ID";
+            // 
+            // status
+            // 
+            this.status.Text = "Order Status";
+            // 
+            // time
+            // 
+            this.time.Text = "Time";
+            // 
+            // KitchenViewcs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "KitchenViewcs";
+            this.Text = "KitchenViewcs";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Id;
+        private System.Windows.Forms.ColumnHeader status;
+        private System.Windows.Forms.ColumnHeader time;
+    }
+}
---
+++ ChapeauUI/KitchenViewcs.cs
@@ -0,0 +1,26 @@
+﻿using ChapeauLogic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChapeauUI
+{
+    public partial class KitchenViewcs : Form
+    {
+        public KitchenViewcs()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadOrders()
+        {
+            OrderService orderService = new OrderService();
+
+            List<Order> orders = new List<Order>();
+        }
+    }
+}
