486a9fb24706b175d8b16d921916243704c6335c
sasabeghiu <70911600+sasabeghiu@users.noreply.github.com>
2021-06-11 15:33:19 +0200

    update
    added Celeste's code + class diagram

--- ChapeauUI/Bill_PaidInCash.cs
+++ ChapeauUI/Bill_PaidInCash.cs
@@ -12,24 +12,27 @@ namespace ChapeauUI
 {
     public partial class Bill_PaidInCash : Form
     {
+        BillService bill;
+        int total;
         public Bill_PaidInCash()
         {
             InitializeComponent();
+            bill = new BillService();
+
+            lbl_total.Text = $"{bill.CalculateBill()}";
+            total = int.Parse(lbl_total.Text);
         }
 
         private void btn_confirm_Click(object sender, EventArgs e)
         {
-            (new Bill()).Show(); this.Close();
+            int received = int.Parse(txtBox_Handin.Text);
+            int tip = int.Parse(txtBox_Tip.Text);
+            lbl_change.Text = $"{received - total - tip}";
         }
 
         private void Bill_PaidInCash_Load(object sender, EventArgs e)
         {
-            BillService bill = new BillService();
-            lbl_total.Text = $"{bill.CalculateBill()}";
-            int total = int.Parse(lbl_total.Text);
-            int receveid = int.Parse(txtBox_Handin.Text);
-            int tip = int.Parse(txtBox_Tip.Text);
-            lbl_change.Text = $"{receveid - total - tip}";
+            
         }
     }
 }
